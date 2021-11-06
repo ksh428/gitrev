39bd4f50a2c176c9531ad06748886073d73bc6ae
Kshounish Chakraborty <kshounish03@gmail.com>
2021-11-06 08:58:34 +0530

    first commit

---
+++ gittt.cpp
@@ -0,0 +1,37 @@
+
+#include<bits/stdc++.h>
+#define ll long long
+#define int long long
+#define mod 1000000007
+#define endl "\n"
+using namespace std;
+
+void initcode() {
+#ifndef ONLINE_JUDGE
+
+	freopen("input.txt", "r", stdin);
+
+	freopen("output.txt", "w", stdout);
+
+#endif // ONLINE_JUDGE
+}
+
+void solve() {
+    cout<<"ok i am here";
+
+
+}
+
+
+int32_t main() {
+	initcode();
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
